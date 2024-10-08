e4d6e91a8a60fa186204413f7bcec995e7f042a9
Airat Karimov <frigesty@gmail.com>
2024-10-08 17:49:04 +0300

    edit shortened the search text

--- src/test/java/SelenideTest.java
+++ src/test/java/SelenideTest.java
@@ -5,14 +5,14 @@ import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 import static com.codeborne.selenide.Selenide.open;
 
-public class SelenideTest {@Test
-void practiceFormTest(){
-    open("https://github.com/selenide/selenide");
-    $("#wiki-tab").click();
-    $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
-    $(".filterable-active").shouldHave(Condition.text("SoftAssertions"));
-    $("[data-filterable-for=wiki-pages-filter]").$(byText("SoftAssertions")).click();
-    $(".markdown-body").shouldHave(Condition.text("Using JUnit5 extend test class"));
-
-}
+public class SelenideTest {
+    @Test
+        void practiceFormTest(){
+            open("https://github.com/selenide/selenide");
+            $("#wiki-tab").click();
+            $("#wiki-pages-filter").setValue("SoftAssertions").pressEnter();
+            $(".filterable-active").shouldHave(Condition.text("SoftAssertions"));
+            $("[data-filterable-for=wiki-pages-filter]").$(byText("SoftAssertions")).click();
+            $(".markdown-body").shouldHave(Condition.text("Using JUnit5 extend test "));
+    }
 }
